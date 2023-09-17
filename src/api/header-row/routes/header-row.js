0af34056aeaebaec61c53dfe882536bd857826bb
Rishan Karunarathna <r.karunarathna@sana-commerce.com>
2023-09-18 03:35:06 +0530

    mega menu first stage

---
+++ src/api/header-row/routes/header-row.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * header-row router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::header-row.header-row');
---
+++ src/api/header-row/services/header-row.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * header-row service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::header-row.header-row');
